ee58bc28b6bf89cc881e06ce963986fc372bb4b0
chaitey50 <chaiteyc@gmail.com>
2024-09-30 02:05:16 +0600

    alpha clash

---
+++ script/alpha-clash.js
@@ -0,0 +1,10 @@
+function play(){
+   hideElementById('home-screen');
+   showElementById('play-ground')
+   continueGame(); 
+}
+
+function continueGame(){
+const alphabet = getARandomAlphabet();
+}
+
---
+++ script/utility.js
@@ -0,0 +1,21 @@
+function hideElementById(elementId){
+const element = document.getElementById(elementId);
+element.classList.add('hidden')
+}
+function showElementById(elementId){
+    const element=document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+function getARandomAlphabet(){
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    console.log(alphabets);
+    
+    const radomNumber =Math.random()*25;
+    const index = Math.round(radomNumber);
+   
+    const alphabet =alphabets[index];
+    console.log(index);
+    return(alphabet)
+    
+}
